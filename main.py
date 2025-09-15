c4bfd66133c0ab21c689c57b3ade98875577f888
macrespo42 <macrespo@student.42.fr>
2025-09-15 13:03:49 +0200

    feat: add verbose mode

--- main.py
+++ main.py
@@ -2,23 +2,28 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
-def main(prompt: str):
+def main(prompt: str, verbose=False):
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=prompt
+        model="gemini-2.0-flash-001", contents=messages
     )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
-        main(sys.argv[1])
+        verbose = len(sys.argv) == 3 and sys.argv[2] == "--verbose"
+        main(sys.argv[1], verbose=verbose)
     else:
         print("Please provide a prompt as script argument")
         exit(1)
