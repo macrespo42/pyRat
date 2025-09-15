db1898b24710af14af2bd3956a6c01080b169110
macrespo42 <macrespo@student.42.fr>
2025-09-15 21:32:37 +0200

    feat: add all other functions (read/write files) and exec python script

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,21 @@
 import os
 from config import FILE_SIZE_LIMIT
+from google.genai import types
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {FILE_SIZE_LIMIT} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
 
 
 def get_file_content(workding_directory: str, file_path: str) -> str:
