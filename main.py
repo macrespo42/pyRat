b464f5c5fa99145f8791090a08a9ae6341c5c02a
macrespo42 <macrespo@student.42.fr>
2025-09-15 21:11:48 +0200

    feat: tell the agent he case use get_files_infos

--- main.py
+++ main.py
@@ -3,8 +3,26 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_infos import schema_get_files_info
 
-system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+
+def get_available_functions():
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+    return available_functions
 
 
 def main(prompt: str, verbose=False):
@@ -15,9 +33,15 @@ def main(prompt: str, verbose=False):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[get_available_functions()], system_instruction=system_prompt
+        ),
     )
-    print(response.text)
+    if response.function_calls:
+        for call in response.function_calls:
+            print(f"Calling function: {call.name}({call.args})")
+    else:
+        print(response.text)
     if verbose:
         print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
