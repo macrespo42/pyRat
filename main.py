70c547e92d56586eefbc8626a9ce906165a01faf
macrespo42 <macrespo@student.42.fr>
2025-09-16 14:35:01 +0200

    feat: ai agent is operationnal

--- main.py
+++ main.py
@@ -40,29 +40,47 @@ def main(prompt: str, verbose=False):
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[get_available_functions()], system_instruction=system_prompt
-        ),
-    )
-    if response.function_calls:
-        for call in response.function_calls:
-            print(f"Calling function: {call.name}({call.args})")
-            result = call_function(call)
-            if not result.parts[0].function_response.response:
-                raise SystemError(
-                    f"Fatal error when executing {call.name} function with {call.args} arguments"
-                )
+
+    try:
+        for _ in range(0, 20):
+            response = client.models.generate_content(
+                model="gemini-2.0-flash-001",
+                contents=messages,
+                config=types.GenerateContentConfig(
+                    tools=[get_available_functions()], system_instruction=system_prompt
+                ),
+            )
             if verbose:
-                print(f"-> {result.parts[0].function_response.response}")
-    else:
-        print(response.text)
-    if verbose:
-        print(f"User prompt: {prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+                print(f"User prompt: {prompt}")
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(
+                    f"Response tokens: {response.usage_metadata.candidates_token_count}"
+                )
+
+            if response.candidates:
+                for candidate in response.candidates:
+                    if candidate.content:
+                        messages.append(candidate.content)
+
+            function_responses = []
+            if response.function_calls:
+                for call in response.function_calls:
+                    print(f"Calling function: {call.name}({call.args})")
+                    result = call_function(call)
+                    if not result.parts[0].function_response.response:
+                        raise SystemError(
+                            f"Fatal error when executing {call.name} function with {call.args} arguments"
+                        )
+                    if verbose:
+                        print(f"-> {result.parts[0].function_response.response}")
+                    function_responses.append(result.parts[0])
+                messages.append(types.Content(role="user", parts=function_responses))
+            else:
+                print("Final response:")
+                print(response.text)
+                break
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 if __name__ == "__main__":
